e6280ae9cc6e40791cde851db87a464954f1a154
Motty <m.mottinelli002@studenti.unibs.it>
2024-09-05 16:45:39 +0200

    feat: implemented base structure for problems

---
+++ app/routers/problem.py
@@ -0,0 +1,20 @@
+from fastapi import APIRouter
+from fastapi.responses import JSONResponse
+
+router = APIRouter(
+    tags=["Problems"],
+    prefix="/problem"
+)
+
+@router.get("/test", summary="Test", response_description="Test")
+async def test():
+    """
+    Test
+
+    Returns:
+        JSONResponse: response
+    """
+    return JSONResponse(
+        status_code=200,
+        content={"detail": "Test successful"}
+    )
