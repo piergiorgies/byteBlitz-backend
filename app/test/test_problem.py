416b1233acc236a299adb72d76ab01bafd8bb1c5
lellopastellone <l.sigalini001@studenti.unibs.it>
2024-10-11 10:19:24 +0200

    added some test implementations. (need to be fixed)

--- app/test/test_problem.py
+++ app/test/test_problem.py
@@ -1,5 +1,6 @@
 import pytest
 import requests
+from app.models.mapping.problem import Problem
 from app.test.mock import admin_headers, user_headers, base_url
 
 url = base_url + 'problems/'
@@ -31,24 +32,67 @@ def test_list_problems():
     # 1. l'admin deve ricevere un json con due parametri: data e count
     # 2. l'utente deve ricevere un json uguale ma con un count inferiore
     # (deve nascondere i problemi non false)
+    response = requests.get(url=url + params, headers=admin_headers, json=data)
+    #assert isinstance(problem, Problem) #TODO: to be fixed. 
+    assert response.json().get("count") == 9
 
+    response = requests.get(url=url + params, headers=user_headers, json=data)
+    assert "data" in response.json() and "count" in response.json()
+    assert response.json().get("count") == 5
     # code_checks
     # [**LEO**] qui invece andiamo a controllare che i controlli implementati
     # a livello di codice funzionino correttamente. In questo caso dobbiamo controllare che
     # modificando limit e offset questi vengano modificati realmente (MA COUNT DEVE RIMANERE UGUALE!!) 
-    
+    limit = 1
+    offset = 2
+    response = requests.get(url=url + params, headers=admin_headers, json=data)
+    assert len(response.json().get("data")) == 1
+    assert response.json().get("count") == 9
+
+    limit = 2
+    offset = 0
+    response = requests.get(url=url + params, headers=admin_headers, json=data)
+    assert len(response.json().get("data")) == 2
+    assert response.json().get("count") == 5
     # edge_cases
     # [**LEO**] qui proviamo tutti i casi limite, principalmente quelli non realizzabili a livello di
     # frontend per rendere l'applicazione robusta (es. offset = count, limit = 0)
 
+    limit = 0
+    offset = 1
+    response = requests.get(url=url + params, headers=admin_headers, json=data)
+    assert response.status_code == 422
+
+    limit = 1
+    offset = -1
+    response = requests.get(url=url + params, headers=admin_headers, json=data)
+    assert response.status_code == 422
+
+    limit = 1
+    offset = 10
+    response = requests.get(url=url + params, headers=admin_headers, json=data)
+    assert response.status_code == 422
+
+    limit = 1
+    offset = "count"
+    response = requests.get(url=url + params, headers=admin_headers, json=data)
+    assert response.status_code == 422
 
 # GET read_problem
 def test_read_problem():
     # authentication
+    response = requests.get(url=url + '3/', headers=admin_headers)
+    assert response.status_code == 200
+    
+    response = requests.get(url=url + '1/', headers=user_headers)
+    assert response.status_code == 200
+
+    response = requests.get(url=url + '3/', headers=user_headers)
+    assert response.status_code == 404
     # base_functionalities
+    response = requests.get(url=url + '3/', headers=admin_headers) #TODO: to be finished.
     # code_checks
     # edge_cases
-    pass
 
 # POST create_problem
 def test_create_problem():
